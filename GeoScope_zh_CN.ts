baa2dbcaa23c390825b4385512807eaf2efbe257
OzzieNicholas <2998622373@qq.com>
2025-01-17 23:07:47 +0800

    chore: initialize project structure

--- GeoScope_zh_CN.ts
+++ GeoScope_zh_CN.ts
@@ -1,3 +1,13 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/GeoScope_autogen/include/ui_MainWindow.h" line="51"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
